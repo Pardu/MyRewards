cd80782bed48247f5e3b7a2961ffc5511bda415b
Rajashekar Pilli <38795188+Pardu@users.noreply.github.com>
2022-11-01 17:59:09 +0530

    Delete src folder

--- src/components/ui/Text.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { colors, SPACING } from '../../utils/theme'
-// CarmenSans-Regular, Roboto-Regular, CocoGothic, CocoGothic-Bold
-export default props => <Text {...props} style={[{ fontFamily: 'CarmenSans-Regular', color: colors.dark, fontSize: SPACING * 1.2 }, props.style]}>{props.children}</Text>
